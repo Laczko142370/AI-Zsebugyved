11324fbb15e44d79dd3864cf9615aeb202621614
Komlosi Laszlo <laczko142370@hotmail.com>
2025-02-08 21:08:14 +0100

    Frissítés: Flask futtatási mód módosítva Railway számára

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def auth_callback():
     return render_template("index.html", name=user_info["name"], picture=user_info["picture"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
